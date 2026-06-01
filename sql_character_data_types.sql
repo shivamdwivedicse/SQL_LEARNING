07c6a0ded6e612b96848e1ca7d65c6fe99cc7006
Shivam Dwivedi <shivamdwivedicse20919@gmail.com>
2026-06-01 09:33:50 +0530

    sql_character_data_types

---
+++ sql_character_data_types.sql
@@ -0,0 +1,11 @@
+create table strings (
+code char(5),
+email varchar(100),
+bio text
+);
+
+INSERT into strings
+VALUES ('23vb4' , 'shivam@gmail.com' , 'Hello i am a student of sheriyas AI school');
+
+SELECT * from strings;
+
